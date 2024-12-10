8c9ae280d3a31eb53775d8b91cf11e1d454e49ac
KingHarley <cweiss1995@gmail.com>
2024-12-10 22:54:33 +0800

    Add Dal Project

---
+++ DAL/CarpoolingDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using DAL.Models;
+using System.Runtime.CompilerServices;
+
+
+namespace DAL
+{
+    internal class CarpoolingDbContext : DbContext
+    {
+        DbSet<Event> Events { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Event>();
+        }
+    }
+}
---
+++ DAL/Models/Event.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Models
+{
+    public class Event
+    {
+        long Id { get; set; }
+        string Name { get; set; } = null!;
+        DateTimeOffset StartTime { get; set; }
+        DateTimeOffset EndTime { get; set; }
+
+    }
+}
